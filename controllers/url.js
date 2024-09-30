0730a2c6b26f42f2ce0a9cdaf8dc0458325b6ba8
Chahat <chahatj06@gmail.com>
2024-10-01 03:34:03 +0530

    Url Exist error

--- controllers/url.js
+++ controllers/url.js
@@ -5,7 +5,15 @@ async function handleGenerateNewShortUrl (req,res) {
 
 const body = req.body;
 if(!body.url) return res.status(400).json({error:'URL is required'});
-
+    //check if url already exits in database
+    const existingUrl = await URL.findOne({redirectURL:body.url})
+    // if exist send an alert that url already exist in the homepage
+    if(existingUrl) return res.status(400).render('home',{
+        error:'URL already exist',
+        
+    })
+    // if(existingUrl) return res.status(400).json({error:'URL already exist'})
+    
     const shortID=shortid();
     await URL.create({
         shortId:shortID,
